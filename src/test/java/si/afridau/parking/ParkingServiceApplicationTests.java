181903abaaf74b3ac0c789cc50a7c960fbfdfa9b
FrostyKitten02 <alen.fridau02@gmail.com>
2025-03-23 20:04:19 +0100

    initial spring boot setup

---
+++ src/test/java/si/afridau/parking/ParkingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package si.afridau.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
